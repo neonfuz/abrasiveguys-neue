0e2bbc670555ad599b734e5d4df7ac81ea4635bd
Sage Raflik <neonfuz@gmail.com>
2016-04-19 12:08:57 -0500

    Started to implement magellan in nav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,12 +4,22 @@ var tabTemplate = Handlebars.compile('<li class="tabs-title"><a href="#{{id}}">{
 
 $('.tabs-content').children('.tabs-panel').each(function(index, item) {
     var element = tabTemplate({id: item.id, text: ''});
-    if( $(item).hasClass('is-active') ) {
+    item = $(item);
+    if( item.hasClass('is-active') ) {
         element = $(element);
         element.addClass('is-active');
         element.children('a').attr('aria-selected', 'true');
     }
     tabs.append(element);
+});
+
+$('#accordion-nav').find('a.accordion-title').each(function(index, item) {
+    var navItem = $(item);
+    var pageItem = $(navItem.attr('href'))
+
+    pageItem.children('section').each(function(index, item) {
+        // Todo
+    });
 });
 
 $(document).foundation();
