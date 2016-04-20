e5aba60a58ade90d72e5a2bbf2473584e48b7c94
Sage Raflik <neonfuz@gmail.com>
2016-04-19 22:18:26 -0500

    Multiple fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,30 +1,21 @@
-var tabs = $('#tabs');
-
 var tabTemplate = Handlebars.compile('<li class="tabs-title"><a href="#{{id}}">{{text}}</a></li>');
 
 $('.tabs-content').children('.tabs-panel').each(function(index, item) {
     var element = tabTemplate({id: item.id, text: ''});
-    item = $(item);
-    if( item.hasClass('is-active') ) {
-        element = $(element);
-        element.addClass('is-active');
-        element.children('a').attr('aria-selected', 'true');
+
+    if( $(item).hasClass('is-active') ) {
+        $(element)
+            .addClass('is-active')
+            .children('a').attr('aria-selected', 'true');
     }
-    tabs.append(element);
-});
 
-$('#accordion-nav').find('a.accordion-title').each(function(index, item) {
-    var navItem = $(item);
-    var pageItem = $(navItem.attr('href'))
-
-    pageItem.children('section').each(function(index, item) {
-        // Todo
-    });
+    $('#tabs').append(element);
 });
 
 $(document).foundation();
 
 $('#accordion-nav').children('.accordion-item').click(function() {
-    var href = $(this).children('a').attr('href');
-    $('#tabs').foundation('selectTab', href.substr(1));
+    var href = $(this).children('a').attr('href').substr(1);
+    $('#tabs').foundation('selectTab', href);
+    window.location.hash = href;
 });
